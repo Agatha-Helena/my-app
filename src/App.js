d5958564eab6e70a7fd9e2e46d8ba993b69d989e
Agatha-Helena <aghata.m@aluno.senai.br>
2026-02-20 13:50:09 -0300

    aula4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { ExpressoesJSX } from './jsx_examples/02_expressoes_jsx';
+/* import { ExpressoesJSX } from './jsx_examples/02_expressoes_jsx'; */
+/* import Exemplo03 from './jsx_examples/03_condicionais_jsx'; */
+import Exemplo04 from './jsx_examples/04_listas_map_jsx';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ExpressoesJSX/>
+      <Exemplo04/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'turquoise',
+    backgroundColor: '#c5c5c5',
     alignItems: 'center',
     justifyContent: 'center',
   },
